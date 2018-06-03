3296a87b87310982be674aa4ded9f7584757bfb6
William Li <wul@microsoft.com>
2018-06-03 11:19:14 -0700

    Init from template

---
+++ BussinessRule/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+
+namespace BussinessRule
+{
+    public interface IGood
+    {
+    }
+
+    public class Book : IGood
+    {
+        public Book(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public class MembershipGood : IGood
+    {
+        public MembershipGood(string membership)
+        {
+            Membership = membership;
+        }
+
+        public string Membership { get; }
+    }
+
+    public interface IRule
+    {
+        IReadOnlyCollection<ICommand> Handle(IGood good);
+    }
+
+    public interface ICommand
+    {
+    }
+
+
+    public class SlipForShippingRule : IRule
+    {
+        public IReadOnlyCollection<ICommand> Handle(IGood good)
+        {
+            if (good is Book)
+                return new[] {new SlipCommand("Shipping", good)};
+
+            return new ICommand[0];
+        }
+    }
+
+    public class SlipCommand : ICommand
+    {
+        public SlipCommand(string action, IGood good)
+        {
+            Action = action;
+            Good = good;
+        }
+
+        public string Action { get; }
+        public IGood Good { get; }
+    }
+
+
+    public class ActivateRule : IRule
+    {
+        public IReadOnlyCollection<ICommand> Handle(IGood good)
+        {
+            var m = good as MembershipGood;
+            if (m != null)
+                return new[] {new ActivateCommand(m.Membership)};
+
+            return new ICommand[0];
+        }
+    }
+
+    public class ActivateCommand : ICommand
+    {
+        public ActivateCommand(string membership)
+        {
+            Membership = membership;
+        }
+
+        public string Membership { get; }
+    }
+
+
+    public class CompositeRule : IRule
+    {
+        private readonly IRule[] rules;
+
+        public CompositeRule(params IRule[] rules)
+        {
+            this.rules = rules;
+        }
+
+        public IReadOnlyCollection<ICommand> Handle(IGood good)
+        {
+            var commands = new List<ICommand>();
+            foreach (var rule in rules)
+                commands.AddRange(rule.Handle(good));
+            return commands;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ BussinessRuleTest/BussinessRuleTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using BussinessRule;
+using Xunit;
+
+namespace FeedCheckerTest
+{
+    public class GivenFeedChecker
+    {
+        [Fact]
+        public void BooksTest()
+        {
+            var rule =
+                new CompositeRule(
+                    new SlipForShippingRule(),
+                    new ActivateRule());
+
+            var book = new Book("The Annotated Turing");
+            var commands = rule.Handle(book);
+            var command = commands.First() as SlipCommand;
+            Assert.Equal(book, command.Good);
+            Assert.Equal("Shipping", command.Action);
+        }
+
+        [Fact]
+        public void MembershipTests()
+        {
+            var rule =
+                new CompositeRule(
+                    new SlipForShippingRule(),
+                    new ActivateRule());
+
+            var membership = new MembershipGood("John Smith");
+            var commands = rule.Handle(membership);
+            var command = commands.First() as ActivateCommand;
+            Assert.Equal("John Smith", command.Membership);
+        }
+    }
+}
